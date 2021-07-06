884d02dcd4aab7217b516dbb823979387b205d0c
Hamza-beta <85179836+Hamza-beta@users.noreply.github.com>
2021-07-06 20:28:35 +0100

    testing

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import Header from './components/header'
+import FormAdd from './components/formAdd'
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <FormAdd/>
+
+        </div>
+  )
+  
+}
+
+export default App
